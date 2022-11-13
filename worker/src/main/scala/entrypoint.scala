3982d1be98ebe46e69c33e96bd82a1aaa6b31f2d
lemonholic <lemonwater@postech.ac.kr>
2022-11-14 02:55:24 +0900

    Make master/worker follow predefined behaviours

--- worker/src/main/scala/entrypoint.scala
+++ worker/src/main/scala/entrypoint.scala
@@ -1,15 +1,22 @@
 import java.net.Socket
 import network.utils.ReadWriteSocket
+import java.net.Inet4Address
+import java.net.InetSocketAddress
 
 object Entrypoint {
   def send_request(rwSocket: ReadWriteSocket) = {
     val message = "message from worker"
     rwSocket.write_string(message)
-    println(rwSocket.read_string(100))
+    val message_from_master = rwSocket.read_string(100).trim
+    require(message_from_master == "message from master")
   }
 
   def main(args: Array[String]) = {
-    val socket = new Socket("localhost", 44123)
+    val (master_host, master_port) = args(0).split(":") match {
+      case Array(host, port) => (host, port.toInt)
+      case _ => throw new Exception("Provide master address")
+    }
+    val socket = new Socket(master_host, master_port)
     val rwSocket = new ReadWriteSocket(socket)
     send_request(rwSocket)
     socket.close()
