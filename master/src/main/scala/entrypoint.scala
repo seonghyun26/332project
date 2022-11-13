b5967c71ec0566f84dad1ba1f9a7b3359dd6a9bc
lemonholic <lemonwater@postech.ac.kr>
2022-11-14 02:55:24 +0900

    Make master/worker follow predefined behaviours

--- master/src/main/scala/entrypoint.scala
+++ master/src/main/scala/entrypoint.scala
@@ -3,20 +3,31 @@ import network.utils.ReadWriteSocket
 
 object Entrypoint {
   def handle_request(rwSocket: ReadWriteSocket) = {
-    println(rwSocket.read_string(100))
+    val message_from_worker = rwSocket.read_string(100).trim
+    require(message_from_worker == "message from worker")
     val message = "message from master"
     rwSocket.write_string(message)
   }
 
-  def create_http_response(content: String) = 
-    s"HTTP/1.1 200 OK\r\nContent-Type: plaintext\r\nContent-Length: ${content.length}\r\n\r\n$content"
-
   def main(args: Array[String]) = {
-    val socket = new ServerSocket(44123)
-    val client_connection = socket.accept()
-    val rwSocket = new ReadWriteSocket(client_connection)
-    handle_request(rwSocket)
-    client_connection.close()
+    val num_workers = args.lift(0) match {
+      case Some(n) => n.toInt
+      case None => throw new Exception("Provide # of workers")
+    }
+    val port = args.lift(1).getOrElse("44123").toInt
+    val socket = new ServerSocket(port)
+    val listen_address = s"${socket.getInetAddress.getHostAddress}:${socket.getLocalPort}"
+    println(listen_address)
+
+    val addresses_of_workers = for {i <- 0 until num_workers} yield {
+      val connection_to_client = socket.accept()
+      val client_address = s"${connection_to_client.getRemoteSocketAddress}".stripPrefix("/")
+      val rwSocket = new ReadWriteSocket(connection_to_client)
+      handle_request(rwSocket)
+      connection_to_client.close()
+      client_address
+    }
+    println(addresses_of_workers.mkString(", "))
     socket.close()
   }
 }
