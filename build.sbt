5a100e126997596db512930fe017585614d93b1b
lemonholic <lemonwater@postech.ac.kr>
2022-11-15 04:19:40 +0900

    Add program correctness checker to CI workflow

--- build.sbt
+++ build.sbt
@@ -5,11 +5,27 @@ ThisBuild / scalacOptions += "-release:11"
 version := "0.1.0"
 name := "DistSort"
 
+lazy val scalatest = "org.scalatest" %% "scalatest" % "3.2.7"
+
 lazy val network = (project in file ("network"))
 lazy val master = (project in file("master"))
-  .settings(
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.7" % Test,
-  )
   .dependsOn(network)
+  .settings(
+    libraryDependencies += scalatest % Test,
+    assembly / assemblyJarName := "master.jar",
+  )
 lazy val worker = (project in file("worker"))
   .dependsOn(network)
+  .settings(
+    libraryDependencies += scalatest % Test,
+    assembly / assemblyJarName := "worker.jar",
+  )
+
+lazy val project_root = (project in file ("."))
+  .aggregate(master, worker, network)
+  .configs(IntegrationTest)
+  .settings(
+    Defaults.itSettings,
+    libraryDependencies += scalatest % "it",
+    run / aggregate := false,
+  )
---
+++ scripts/gensort.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import tempfile
+from typing import Tuple, Iterable
+
+KEY_LENGTH = 10
+VALUE_LENGTH = 90
+HOME = os.getenv('HOME')
+
+def _gen(bytestream: bytes):
+    l = KEY_LENGTH + VALUE_LENGTH
+    for offset in range(0, len(bytestream), l):
+        yield bytestream[offset : offset + l]
+
+def _bytes_to_tuples(bytes_: bytes) -> Iterable[Tuple[bytes, bytes]]:
+    return map(lambda kv: (kv[:KEY_LENGTH], kv[KEY_LENGTH:]), _gen(bytes_))
+
+def generate_tuples_to_file(num_tuples: int, filename: str):
+    subprocess.run([f'{HOME}/gensort/gensort', '-s', str(num_tuples), filename])
+
+def generate_tuples(num_tuples: int) -> Iterable[Tuple[bytes, bytes]]:
+    filename = tempfile.mktemp()
+    generate_tuples_to_file(num_tuples, filename)
+    with open(filename, 'rb') as f:
+        tuples = _bytes_to_tuples(f.read())
+    os.remove(filename)
+    return tuples
+
+def load_tuples(filename: str) -> Iterable[Tuple[bytes, bytes]]:
+    with open(filename, 'rb') as f:
+        return _bytes_to_tuples(f.read())
+
+def sort_in_file(filename: str):
+    tuples = load_tuples(filename)
+    with open(filename, 'wb') as f:
+        for k, v in sorted(tuples):
+            f.write(k + v)
+
+def validate_file(filename: str) -> Tuple[bool, str]:
+    p = subprocess.run([f'{HOME}/gensort/valsort', filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    return p.returncode == 0, p.stdout.decode()
+
+def _get_gensort():
+    git_repo = 'https://github.com/petergeoghegan/gensort.git'
+    os.system(f'git clone {git_repo} {HOME}/gensort && cd {HOME}/gensort && make all')
+
+_get_gensort()
