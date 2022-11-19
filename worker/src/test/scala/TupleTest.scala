267d9e603fe9759acdca05314c2eaf08f3170234
dongyeop3813 <ehdduq1503@gmail.com>
2022-11-19 16:21:02 +0900

    Add test for Tuple, gensort.

--- worker/src/test/scala/TupleTest.scala
+++ worker/src/test/scala/TupleTest.scala
@@ -1,18 +1,53 @@
 package test
 
+import java.lang.IllegalArgumentException
 import org.scalatest.funsuite.AnyFunSuite
 import scala.io.Source
 
 import worker.Tuple
 
+import sys.process._
+
+
 class TupleSuite extends AnyFunSuite {
 
-    test("Tuple Test") {
-        val source = Source.fromFile("./worker/partition1", "ISO8859-1")
+    "./scripts/get-gensort.sh -q" !
+
+    "./gensort/gensort -b 1000 ./temp/partition1" !
+
+    test("Tuple Load Test") {
+
+        val source = Source.fromFile("./temp/partition1", "ISO8859-1")
 
         val byte_list = source.take(100).toList.map {_.toByte}
 
         assert(Tuple(byte_list).key == byte_list.take(10))
         assert(Tuple(byte_list).value == byte_list.drop(10))
+    }
+
+    test("Tuple Spec Test 1") {
+        val byte_list: List[Byte] = (1 to 110).toList.map {_.toByte}
+
+        assertThrows[IllegalArgumentException]{
+            Tuple(byte_list)
+        }
+
+    }
+
+    test("Tuple Comparison Test 1") {
+
+        val byte_list1: List[Byte] = (1 to 100).toList.map {_.toByte}
+        val byte_list2: List[Byte] = (2 to 101).toList.map {_.toByte} 
+
+        assert(Tuple(byte_list1) < Tuple(byte_list2))
+
+    }
+
+    test("Tuple Comparison Test 2") {
+
+        val byte_list1: List[Byte] = (1 to 100).toList.map {_.toByte}
+
+        assert(!(Tuple(byte_list1) < Tuple(byte_list1)))
+
     }
 }
