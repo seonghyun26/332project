5d4236c1ffb341510468dd7c36ae1e474dd710ea
lemonholic <lemonwater@postech.ac.kr>
2022-11-19 16:42:05 +0900

    Change gensort directory

--- scripts/get-gensort.sh
+++ scripts/get-gensort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Git clone and build gensort
-git clone https://github.com/petergeoghegan/gensort.git ~/gensort
-cd ~/gensort
+git clone https://github.com/petergeoghegan/gensort.git ./gensort
+cd ./gensort
 make
