2e1b1901c26dea0e3f140e0431e9f61c79150dd4
soda <60561809+dongyeop3813@users.noreply.github.com>
2022-11-30 04:21:09 +0900

    Modify minor error in worker.

--- worker/src/main/scala/Worker.scala
+++ worker/src/main/scala/Worker.scala
@@ -2,13 +2,46 @@
 package worker
 
 
+import sys.process._
+
+
 object Worker {
 
   val outputDir = "./temp/output"
 
+  def main(args: Array[String]): Unit = {
+    ???
+  }
+
+  def initializeBlocks(fileNameList: List[String]): List[Block] = {
+    val blocks = fileNameList map { fileName:String => new Block(fileName) }
+
+    setUpTempDirectory(blocks)
+
+    blocks
+  }
+
+  def setUpTempDirectory(blocks: List[Block]): Unit = {
+    blocks.zipWithIndex foreach {
+      case (block, id) => {
+
+        val tempDir = s"$outputDir/temp/$id/"
+
+        s"mkdir $tempDir" !
+
+        block.setTempDir(tempDir)
+      }
+    }
+  }
+
+  def sampleFromBlocks(blockList: List[Block], sampleSize: Int): List[Tuple] = {
+    ???
+  }
+
+
   def merge(blocks: List[Block]): List[Block] = {
     val streamList: List[Stream[Tuple]] = blocks map { block => block.toStream }
-    val mergedTupleStream: Stream[Tuple] = Tuple.mergeStream(streamList)
+    val mergedTupleStream: Stream[Tuple] = streamList.mergedStream
     Block.save(outputDir, mergedTupleStream)
   }
 
