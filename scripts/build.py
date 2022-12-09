3dc589d5a150e94f83f86c82d23ce8adbeaa681d
soda <60561809+dongyeop3813@users.noreply.github.com>
2022-12-10 04:20:24 +0900

    Send file to worker

--- scripts/build.py
+++ scripts/build.py
@@ -23,16 +23,28 @@ def putFile(client, file, remote_path):
 if __name__ == "__main__":
     client = createSSHClient('141.223.16.227', '2201', 'cyan', 'cyane4#2002e140b29fc*482a')
 
-    _, stdout, _ = client.exec_command('cd 332project; sbt "master/assembly"')
-    exit_status = stdout.channel.recv_exit_status()
-    if exit_status != 0:
-        raise Exception("Bulid failed")
-
-    _, stdout, _ = client.exec_command('cd 332project; sbt "worker/assembly"')
-    exit_status = stdout.channel.recv_exit_status()
-    if exit_status != 0:
-        raise Exception("Bulid failed")
-
     getFile(client, '/home/cyan/332project/worker/target/scala-2.12/worker.jar', '/home/cyan/worker.jar')
     getFile(client, '/home/cyan/332project/master/target/scala-2.12/master.jar', '/home/cyan/master.jar')
 
+    getFile(client, '/home/cyan/332project/scripts/worker.py', '/home/cyan/worker.py')
+
+    client.close()
+
+    machines = [
+        {
+            'IP': '141.223.16.227',
+            'port': str(i)
+        }
+        for i in range(2203, 2212, 1)
+    ]
+
+    print(machines)
+
+    for machine in machines:
+        client = createSSHClient(machine['IP'], machine['port'], 'cyan', 'cyane4#2002e140b29fc*482a')
+
+        putFile(client, '/home/cyan/worker.jar', '/home/cyan/worker.jar')
+        putFile(client, '/home/cyan/master.jar', '/home/cyan/master.jar')
+        putFile(client, '/home/cyan/worker.py', '/home/cyan/worker.py')
+
+        client.close()
