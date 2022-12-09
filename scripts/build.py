b6d11dbd553b1a67ef2db221fafb8afcc209c129
soda <60561809+dongyeop3813@users.noreply.github.com>
2022-12-10 03:51:04 +0900

    Minor bug fix

--- scripts/build.py
+++ scripts/build.py
@@ -23,9 +23,8 @@ def putFile(client, file, remote_path):
 if __name__ == "__main__":
     ssh = createSSHClient('141.223.16.227', '2201', 'cyan', 'cyane4#2002e140b29fc*482a')
 
-    ssh.exec_command('cd 332project')
-    ssh.exec_command('sbt "master/assembly"')
-    ssh.exec_command('sbt "worker/assembly"')
+    ssh.exec_command('cd 332project; sbt "master/assembly"')
+    ssh.exec_command('cd 332project; sbt "worker/assembly"')
 
     getFile(ssh, '/home/cyan/332project/worker/target/scala-2.12/worker.jar', '/home/cyan/worker.jar')
     getFile(ssh, '/home/cyan/332project/master/target/scala-2.12/master.jar', '/home/cyan/master.jar')
