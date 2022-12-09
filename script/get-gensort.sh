1b932d1ed00dd8d9092815aab97e8dd18a091aaf
lemonholic <lemonwater@postech.ac.kr>
2022-12-10 07:03:56 +0900

    Etc - Get gensort from wget

--- script/get-gensort.sh
+++ script/get-gensort.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Git clone and build gensort
-git clone https://github.com/petergeoghegan/gensort.git ./gensort
-cd ./gensort
-make
+wget http://www.ordinal.com/try.cgi/gensort-linux-1.5.tar.gz
+tar -xzf gensort-linux-1.5.tar.gz && rm -f gensort-linux-1.5.tar.gz
+
+# # Git clone and build gensort
+# git clone https://github.com/petergeoghegan/gensort.git ./gensort
+# cd ./gensort
+# make
