fc44ccb45b5d37ef530bf77408ac39a12f5e0b57
lemonholic <lemonwater@postech.ac.kr>
2022-12-10 06:19:54 +0900

    Test/Fix - Not send master.jar to workers

--- script/multi-machine/setup.py
+++ script/multi-machine/setup.py
@@ -11,18 +11,18 @@ def setup_master():
     exec_command_blocking(ssh, 'git clone https://github.com/seonghyun26/332project.git')
     exec_command_blocking(ssh, f'cd /home/cyan/332project && {SBT_BIN}/sbt "master/assembly"')
     exec_command_blocking(ssh, f'cd /home/cyan/332project && {SBT_BIN}/sbt "worker/assembly"')
+    exec_command_blocking(ssh, f'mv /home/cyan/332project/master/target/scala-2.12/master.jar /home/cyan/master.jar')
+    exec_command_blocking(ssh, f'mv /home/cyan/332project/worker/target/scala-2.12/worker.jar /home/cyan/worker.jar')
 
 def setup_workers():
     client = createSSHClient(MASTER_IP_ADDRESS, MASTER_PORT)
     with tempfile.TemporaryDirectory() as tempdir:
-        getFile(client, '/home/cyan/332project/worker/target/scala-2.12/worker.jar', f'{tempdir}/worker.jar')
-        getFile(client, '/home/cyan/332project/master/target/scala-2.12/master.jar', f'{tempdir}/master.jar')
+        getFile(client, '/home/cyan/worker.jar', f'{tempdir}/worker.jar')
         client.close()
         for port in WORKER_PORTS:
             client = createSSHClient(WORKER_IP_ADDRESS, port)
             putFile(client, f'{tempdir}/worker.jar', '/home/cyan/worker.jar')
-            putFile(client, f'{tempdir}/master.jar', '/home/cyan/master.jar')
-            putFile(client, '/home/cyan/332project/worker.py', '/home/cyan/worker.py')
+            putFile(client, '/home/cyan/332project/script/multi-machine/worker.py', '/home/cyan/worker.py')
             client.close()
 
 def setup_machines():
