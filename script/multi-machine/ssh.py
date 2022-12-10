8511afd898bac344d15fb761f088de3cc1c050e9
lemonholic <lemonwater@postech.ac.kr>
2022-12-11 07:11:35 +0900

    Test - Support for printed remote command execution

--- script/multi-machine/ssh.py
+++ script/multi-machine/ssh.py
@@ -1,13 +1,23 @@
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
+from typing import Tuple
 
 USER = 'cyan'
 KEYFILE = '/home/cyan/.ssh/id_rsa'
 
-def exec_command_blocking(ssh: SSHClient, command: str):
+def exec_command_blocking(ssh: SSHClient, command: str) -> Tuple[str, str]:
     _, stdout, stderr = ssh.exec_command(command)
     return stdout.read().decode(), stderr.read().decode()
+
+def exec_command_printing(ssh: SSHClient, command: str) -> Tuple[str, str]:
+    _, stdout, stderr = ssh.exec_command(command)
+    stdout_acc = ''
+    while not (stdout.channel.closed and stderr.channel.closed):
+        line = stdout.readline()
+        stdout_acc += line
+        print(line, end='')
+    return stdout_acc, stderr.read().decode()
 
 def createSSHClient(server, port):
     ssh = SSHClient()
