932cbe5f71ceadfedc79a7dcfea166cffce2fae0
soda <60561809+dongyeop3813@users.noreply.github.com>
2022-12-11 19:52:46 +0900

    Modify Key.less function

--- common/src/main/scala/Key.scala
+++ common/src/main/scala/Key.scala
@@ -20,13 +20,7 @@ class Key(val value: String) extends Comparable[Key]{
 
   override def <(other: Key): Boolean = {
     require(length == other.length)
-
-    def less(a: String, b: String): Boolean = {
-      if (a.isEmpty && b.isEmpty) return false
-      else (a.head < b.head) || (a.head == b.head && less(a.tail, b.tail))
-    }
-
-    less(value, other.value)
+    value < other.value
   }
 
   override def ==(other: Key): Boolean = {
